4bc413fbdecaab4107c67439d33efdd1af3f56bc
naoyasakurai <sakurainaoya@sakurainaoyanoMacBook-puro.local>
2018-07-26 14:29:09 +0900

    maek_db_by_mysql

--- Gemfile
+++ Gemfile
@@ -4,7 +4,8 @@ source 'https://rubygems.org'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.6'
 # Use sqlite3 as the database for Active Record
-gem 'sqlite3'
+# gem 'sqlite3'
+gem 'mysql2', '>= 0.3.18', '< 0.5'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
@@ -35,7 +36,6 @@ gem 'devise'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
-  gem 'mysql2'
   gem 'byebug'
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -14,35 +14,35 @@
 ActiveRecord::Schema.define(version: 20180726031103) do
 
   create_table "users", force: :cascade do |t|
-    t.string   "name",                   default: "",  null: false
-    t.string   "username",               default: "",  null: false
-    t.string   "email",                  default: "",  null: false
-    t.string   "encrypted_password",     default: "",  null: false
-    t.string   "about"
-    t.string   "location"
-    t.string   "website"
-    t.string   "avator"
-    t.string   "cover"
-    t.string   "color"
-    t.string   "tweets_count",           default: "0", null: false
-    t.string   "follows_count",          default: "0", null: false
-    t.string   "followers_count",        default: "0", null: false
-    t.string   "likes_count",            default: "0", null: false
-    t.string   "reset_password_token"
+    t.string   "name",                   limit: 255, default: "",  null: false
+    t.string   "username",               limit: 255, default: "",  null: false
+    t.string   "email",                  limit: 255, default: "",  null: false
+    t.string   "encrypted_password",     limit: 255, default: "",  null: false
+    t.string   "about",                  limit: 255
+    t.string   "location",               limit: 255
+    t.string   "website",                limit: 255
+    t.string   "avator",                 limit: 255
+    t.string   "cover",                  limit: 255
+    t.string   "color",                  limit: 255
+    t.string   "tweets_count",           limit: 255, default: "0", null: false
+    t.string   "follows_count",          limit: 255, default: "0", null: false
+    t.string   "followers_count",        limit: 255, default: "0", null: false
+    t.string   "likes_count",            limit: 255, default: "0", null: false
+    t.string   "reset_password_token",   limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,   null: false
+    t.integer  "sign_in_count",          limit: 4,   default: 0,   null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at",                           null: false
-    t.datetime "updated_at",                           null: false
+    t.string   "current_sign_in_ip",     limit: 255
+    t.string   "last_sign_in_ip",        limit: 255
+    t.datetime "created_at",                                       null: false
+    t.datetime "updated_at",                                       null: false
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["name"], name: "index_users_on_name"
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  add_index "users", ["username"], name: "index_users_on_username"
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["name"], name: "index_users_on_name", using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  add_index "users", ["username"], name: "index_users_on_username", using: :btree
 
 end
