03a33c2c1ad7aa16813416ee2a63337c884d1b42
naoyasakurai <sakurainaoya@sakurainaoyanoMacBook-puro.local>
2018-08-01 19:10:30 +0900

    transition_users_show_and_users_edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
   def index
   end
+
+  def edit
+  end
+
+  def show
+    # @user = User.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root 'tweets#index'
   get 'users/index'
+  resources :users, only: [:index,:edit,:show]
+
 end
