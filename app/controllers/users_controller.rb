c527c70c3a118a0ad7fc59825222d84d4fdeb251
naoyasakurai <sakurainaoya@sakurainaoyanoMacBook-puro.local>
2018-08-05 13:07:56 +0900

    add_user_serch_function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def index
+     @users = User.where('name LIKE(?)', "%#{params[:keyword]}%").where.not(name: current_user.name)
   end
 
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-
   root 'tweets#index'
   resources :users, only: [:index,:edit,:show,:update]
   resources :tweets, only: [:index, :new, :create]
