93fdcb0af77cebb813977a2faf2fabf0ae939536
naoyasakurai <sakurainaoya@sakurainaoyanoMacBook-puro.local>
2018-08-10 11:29:51 +0900

    エラー解決_@tag定義

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -9,7 +9,6 @@ class Tweet < ActiveRecord::Base
 
 
   mount_uploader :media, MediaUploader
-
   validates :text, presence: true, length: { maximum: 140 } ,unless: :media?
 
   def fav? (fav_user_id, fav_tweet_id)
