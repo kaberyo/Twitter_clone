e083abe268ccbf9f03e34f136d58f6afad4ef155
kaberyo <40202547+kaberyo@users.noreply.github.com>
2018-08-11 13:59:49 +0900

    fixed

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -27,7 +27,7 @@ class TweetsController < ApplicationController
   def destroy
     @tweet = Tweet.find(params[:id])
     @tweet.destroy
-    redirect_to root_path
+    redirect_to (:back)
   end
 
   def search
