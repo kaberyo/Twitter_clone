5704aea81112cdaae85ce73be687c01a59d0c4b2
OkaGouhei <okagouhei@okagoutairanoMacBook-puro.local>
2018-08-16 12:05:19 +0900

    Edit

--- app/assets/javascripts/popup_henshin_iine.js
+++ app/assets/javascripts/popup_henshin_iine.js
@@ -1,33 +1,34 @@
+
+
 $(document).on('turbolinks:load', function(){
- $(function() {
-   $('.tweet__right--favorite').on({
-     'mouseenter':function(){
+  $(function(){
+    $('.tweet__right--reply').on({
+      'mouseenter':function(){
        var dummy = $(this);
        sethover = setTimeout(function(){
-        dummy.find('.iine_popup').fadeIn(200);
-     },500);
+        dummy.find('.reply_popup').fadeIn(200);
+      },500);
     },
-     'mouseleave':function(){
-       $('.tweet__right--favorite').find('.iine_popup').fadeOut(200);
-       clearTimeout(sethover);
-     }
-   });
- });
+      'mouseleave':function(){
+        $('.tweet__right--reply').find('.reply_popup').fadeOut(200);
+        clearTimeout(sethover);
+      }
+    });
+  });
 });
 
 $(document).on('turbolinks:load', function(){
- $(function() {
-   $('.tweet__right--reply').on({
-     'mouseenter':function(){
-       var dummy = $(this);
-       sethover = setTimeout(function(){
-        dummy.find('.henshin_popup').fadeIn(200);
-     },500);
-    },
-     'mouseleave':function(){
-       $('.tweet__right--reply').find('.henshin_popup').fadeOut(200);
-       clearTimeout(sethover);
-     }
-   });
- });
+  $(function(){
+    $('.tweet__right--favorite').on({
+      'mouseenter':function(){
+        var dummy = $(this);
+        sethover = setTimeout(function(){
+          dummy.find('.favorite_popup').fadeIn(200);
+        },500);
+      },'mouseleave':function(){
+        $('.tweet__right--favorite').find('.favorite_popup').fadeOut(200);
+        clearTimeout(sethover);
+      }
+    });
+  });
 });
